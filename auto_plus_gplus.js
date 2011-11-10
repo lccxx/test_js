0cb46d6f0b6c4aab8ce7fda83334c720acd307ea
liu-chong <liuchong14@gmail.com>
2011-11-10 21:20:40 +0800

    Initial commit

---
+++ auto_plus_gplus.js
@@ -0,0 +1,16 @@
+(function() {
+  var jquery_js_src = "http://code.jquery.com/jquery-1.7.min.js";
+  var jquery_doc_script = document.createElement('script');
+  jquery_doc_script.type = 'text/javascript';
+  jquery_doc_script.async = true;
+  jquery_doc_script.src = jquery_js_src;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(jquery_doc_script, s);
+})();
+window.setTimeout(function() {
+  var puls_all_post_comment = function () {
+    $("button[title='Click to +1 this post']").click();
+    $("button[title='Click to +1 this comment']").click();
+  };
+  window.setInterval(puls_all_post_comment, 2000);
+}, 2000);

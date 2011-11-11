7ccd4c82fccfe214ac9458175ecfe3affbb32792
liu-chong <liuchong14@gmail.com>
2011-11-11 09:35:51 +0800

    Change use of load

--- auto_plus_gplus.js
+++ auto_plus_gplus.js
@@ -1,12 +1,13 @@
-(function() {
+(function () {
   var s = document.createElement('script');
   s.type = "text/javascript";
-  s.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js";
+  s.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js";
   var d = document.getElementsByTagName('script')[0];
   d.parentNode.insertBefore(s, d);
-
-  window.setInterval(function() {
-    $("button[title='Click to +1 this post']").click();
-    $("button[title='Click to +1 this comment']").click();
-  }, 2000);
 })();
+
+
+window.setInterval(function () {
+  $("button[title='Click to +1 this post']").click();
+  $("button[title='Click to +1 this comment']").click();
+}, 2000);
--- test_localtion_type.js
+++ test_localtion_type.js
@@ -1,1 +1,13 @@
-alert("Hi!");
+(function() {
+  var s = document.createElement('script');
+  s.type = "text/javascript";
+  s.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js";
+  var d = document.getElementsByTagName('script')[0];
+  d.parentNode.insertBefore(s, d);
+
+  var command = "$(function() { alert('Hi!')} );";
+
+  window.setInterval(function() {
+    $("#a[class='item']").click();
+  }, 2000);
+})();
